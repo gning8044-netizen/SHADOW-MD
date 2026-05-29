a1e1d2b58bc9474db5d2a52d026c0a1894730f3b
Shadow prime <gning8044@gmail.com>
2026-05-29 02:44:16 +0000

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -1,6 +1,6 @@
 async function unmuteCommand(sock, chatId) {
     await sock.groupSettingUpdate(chatId, 'not_announcement'); // Unmute the group
-    await sock.sendMessage(chatId, { text: 'le groupe est fermé allez vous lover.' });
+    await sock.sendMessage(chatId, { text: 'le groupe est ouvert venez mes amies et mes enemies.' });
 }
 
 module.exports = unmuteCommand;
