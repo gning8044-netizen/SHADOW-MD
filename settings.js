3e2c69f9abfb9a6c619f328bbb2b69b65ddbf9c7
Shadow prime <gning8044@gmail.com>
2026-05-29 00:20:46 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: '𝐓𝐎𝐌 𝐏𝐑𝐈𝐌𝐄 𝐗',
+  packname: '𝐃𝐄𝐕 𝐒𝐇𝐀𝐃𝐎𝐖 𝐌𝐃',
   author: '‎',
-  botName: "𝐓𝐎𝐌 𝐏𝐑𝐈𝐌𝐄 𝐗 𝐁𝐎𝐓",
-  botOwner: '𝐏𝐑𝐎𝐅𝐄𝐒𝐒𝐎𝐑 𝐓𝐎𝐌 𝐗', // Your name
-  ownerNumber: '8801892625209', //Set your number here without + symbol, just add country code & number without any space
+  botName: "𝐃𝐄𝐕 𝐒𝐇𝐀𝐃𝐎𝐖 𝐌𝐃 𝐁𝐎𝐓",
+  botOwner: '𝐃𝐄𝐕 𝐒𝐇𝐀𝐃𝐎𝐖 𝐓𝐄𝐂𝐇', // Your name
+  ownerNumber: '‪2348090433713‬', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.7",
-  updateZipUrl: "https://github.com/TOM-PRIME-X/TOM-PRIME-X-BOT/archive/refs/heads/main.zip",
+  updateZipUrl: "",
 };
 
 module.exports = settings;
